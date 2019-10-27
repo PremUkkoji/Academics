9fcabcd6f34acf447aca26074f928e191aba2bc6
Prem Ukkoji <premukkoji@Prems-MacBook-Air.local>
2019-10-27 23:59:20 +0530

    resolved the mess with package.json

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username: string = "";
+  password: string = "";
+  message: string = "";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  logIn(): void{
+    var element = document.getElementById("message");
+
+    if(this.username == "" || this.password == ""){
+      this.message = "All fields are mandatory";
+      element.classList.remove("success-message");
+      element.classList.remove("warning-message");
+      element.classList.add("error-message");
+    }else{
+      this.message = "Success";
+      element.classList.remove("error-message");
+      element.classList.remove("warning-message");
+      element.classList.add("success-message");
+    }
   }
 
 }
